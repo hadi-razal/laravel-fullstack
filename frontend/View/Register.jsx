db27969aaf830b106be354cb000c5fbe409d3d44
Hadi_Razal <hadhirasal22@gmail.com>
2025-07-31 22:38:47 +0400

    created  axiox client file

--- frontend/View/Register.jsx
+++ frontend/View/Register.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
+
+
 
 
   const onSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted");
+
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      confirmPassword: confirmPasswordRef.current.value
+    };
+
   }
 
 
@@ -15,15 +30,15 @@ const Register = () => {
       <div className='form'>
         <form action="" onSubmit={onSubmit}>
           <h1> Create your account</h1>
-          <input type="text" placeholder='Full Name' />
-          <input type="email" placeholder='Email' />
-          <input type="password"  placeholder='Password'/>
-          <input type="password"  placeholder='Re-enter the Password'/>
+          <input ref={nameRef} type="text" placeholder='Full Name' />
+          <input ref={emailRef} type="email" placeholder='Email' />
+          <input ref={passwordRef} type="password" placeholder='Password' />
+          <input ref={confirmPasswordRef} type="password" placeholder='Re-enter the Password' />
           <button className='btn btn-block'>Register</button>
         </form>
 
         <p className='message'>
-         Already have an account? <Link to="/login">login</Link>
+          Already have an account? <Link to="/login">login</Link>
         </p>
 
       </div>
---
+++ frontend/src/axiosClient.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+
+
+const AxiosClient = axios.create({
+  baseURL: process.env.VITE_API_BASE_URL || 'http://localhost:8000/api'
+});
+
+
+AxiosClient.interceptors.request.use((config) => {
+
+    const token = localStorage.getItem('token');
+
+    if (token) {  
+        config.headers['Authorization'] = `Bearer ${token}`;
+    } 
+
+    return config;
+})
+
+AxiosClient.interceptors.response.use((response)=>{
+    return response;
+}, (error) => {
+    const {response} = error;
+    if (response.status === 401) {
+        localStorage.removeItem('token');
+    }
+
+    throw error;
+})
+
+
+export default AxiosClient;
